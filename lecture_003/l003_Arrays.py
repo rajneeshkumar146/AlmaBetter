e2704c3ee5588bb25b39e7bab5c31fff8b82bf39
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-08-06 12:59:50 +0530

    update

--- lecture_003/l003_Arrays.py
+++ lecture_003/l003_Arrays.py
@@ -1,12 +1,16 @@
 import numpy as np
+from sqlalchemy import true
+
 
 def inputEle_01():
     n = int(input())
-    a = list(map(int,input().split()))   # take input in map and mapped to list 
+    # take input in map and mapped to list
+    a = list(map(int, input().split()))
 
-    arr = np.array(a) # convert list into array
+    arr = np.array(a)  # convert list into array
     return arr
-    
+
+
 def inputEle_02():
     a = []
     n = int(input())
@@ -15,7 +19,8 @@ def inputEle_02():
 
     arr = np.array(a)   # convert list into array
     print(arr)
-    
+
+
 def output(arr):
     l = len(arr)
     for i in range(l):
@@ -23,17 +28,60 @@ def output(arr):
 
 
 def maximumEle(arr):
-    ????
+    l = len(arr)
+    if l == 0:
+        return -math.inf
+
+    max = arr[0]
+    for i in range(l):
+        if arr[i] > max:
+            max = arr[i]
+
+    return max
+
 
 def minimum(arr):
-    ???
+    l = len(arr)
+    if l == 0:
+        return math.inf
 
-def linearSearch(arr,data):
-    ???
+    min = arr[0]
+    for i in range(l):
+        if arr[i] < min:
+            min = arr[i]
+
+    return min
 
 
+# T: O(N), where N is No of element, S: O(1)
+def linearSearch(arr, data):
+    l = len(arr)
+
+    for i in range(l):
+        if arr[i] == data:
+            return True
+
+    return False
+
+# T: O(Log(N)), where N is No of element, S: O(1)
+
+
+def binarySearch(arr, data):
+    l = len(arr)
+    si = 0
+    li = l - 1
+
+    while si <= ei:
+        mid = (si + ei) // 2
+        if mid == data:
+            return mid
+        elif arr[mid] < data:
+            si = mid + 1
+        else:
+            ei = mid - 1
+
+    return -1
 
 
 arr = inputEle_01()
 output(arr)
-
