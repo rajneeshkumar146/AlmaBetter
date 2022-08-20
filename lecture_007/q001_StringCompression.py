ed61f1c71e058775e247253586e1bb17b31d8cc9
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-08-20 10:47:34 +0530

    update

---
+++ lecture_007/q001_StringCompression.py
@@ -0,0 +1,44 @@
+# ch is character
+def isLowerCase(ch):
+    return ch >= 'a' and ch <= 'z'
+
+# ch is character
+
+
+def isUpperCase(ch):
+    return ch >= 'A' and ch <= 'Z'
+
+
+# ch is character
+def isDigit(ch):
+    return ch >= '0' and ch <= '9'
+
+
+def convertSmallCharacterLarge(ch):
+    return chr(ord(ch) - ord('a') + ord('A'))
+
+
+def convertLargeCharacterSmall(ch):
+    return chr(ord(ch) - ord('A') + ord('a'))
+
+# T: O(N)
+def upper(str):
+    l = len(str)
+    ans = ""
+
+    for i in range(l):
+        if isLowerCase(str[i]):
+            ans += convertSmallCharacterLarge(str[i])
+        else:
+            ans += str[i]
+
+    return ans
+
+
+def lower():
+
+
+
+# akASD1543*&^ -> AKasd1543*&^
+# small -> Large, Large -> small, rest will same
+def toggleCharcters():
