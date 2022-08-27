812fcb94546f0a1bb89250f00c5a9152152febbb
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-08-27 11:34:13 +0530

    update

--- lecture_009/l001.py
+++ lecture_009/l001.py
@@ -1,4 +1,7 @@
+import math
 # https://codingbat.com/prob/p192383
+
+
 def count8(num):
     if num == 0:
         return 0
@@ -23,24 +26,52 @@ def countHi(str):
 
 # https://codingbat.com/prob/p143900
 def countHi2(str):
-
+    return 1
 
 
 # https://codingbat.com/prob/p170924
 def changePI(str):
-
+    return 1
 
 
 # display List using Recursion
-def display(arr):
+def display(arr, idx):
+    if idx == len(arr):
+        return
+
+    print(arr[idx])
+    display(arr, idx + 1)
 
 
 # display List in reverse order using Recursion
-def displayReverse(arr):
+def displayReverse(arr, idx):
+    if idx == len(arr):
+        return
+
+    displayReverse(arr, idx + 1)
+    print(arr[idx])
 
 
-def findData(arr,data):
+def findData(arr, idx, data):
+    if idx == len(arr):
+        return False
+
+    return arr[idx] == data or findData(arr, idx + 1, data)
 
 
-def maximum(arr):
-    
+def maximum(arr, idx):
+    if idx == len(arr):
+        return -math.inf
+
+    return max(arr[idx], maximum(arr, idx + 1))
+
+# https://leetcode.com/problems/fibonacci-number/
+
+
+def fibo(n):
+    return 1
+
+
+# https://leetcode.com/problems/climbing-stairs/
+def climbingStairs():
+    return 1
