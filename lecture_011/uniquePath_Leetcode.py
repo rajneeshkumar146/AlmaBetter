39b501a6cb4cb777050f2ede9e4bd8fd67f636e2
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-03 13:04:27 +0530

    update

--- lecture_011/uniquePath_Leetcode.py
+++ lecture_011/uniquePath_Leetcode.py
@@ -1,15 +1,22 @@
-def mazePath(sr, sc, er, ec, ans):
-    if sr == er and sc == ec:
-        return 1
+class Solution:
+    def UniquePath(self,sr, sc, er, ec,obstacleGrid):
+        if sr == er and sc == ec:
+            return 1
 
-    count = 0
-    if sr + 1 <= er:  # Horizontal
-        count += mazePath(sr + 1, sc, er, ec)
+        count = 0
+        if sr + 1 <= er and obstacleGrid[sr + 1][sc]  == 0:  # Horizontal
+            count += self.UniquePath(sr + 1, sc, er, ec,obstacleGrid)
 
-    if sr + 1 <= er and sc + 1 <= ec:  # Diagonal
-        count += mazePath(sr + 1, sc + 1, er, ec)
+        if sc + 1 <= ec and obstacleGrid[sr][sc + 1]  == 0:  # Vertical
+            count += self.UniquePath(sr, sc + 1, er, ec,obstacleGrid)
 
-    if sc + 1 <= ec:  # Vertical
-        count += mazePath(sr, sc + 1, er, ec)
+        return count
 
-    return count
+
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        er = len(obstacleGrid) - 1
+        ec = len(obstacleGrid[0]) - 1
+        if obstacleGrid[0][0] == 1 or obstacleGrid[er][ec] == 1:
+            return 0
+        
+        return self.UniquePath(0,0,er,ec,obstacleGrid)
