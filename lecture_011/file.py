d2e33d02a122e6227ff462af786ab628b13ac63f
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-03 11:27:00 +0530

    update

--- lecture_011/file.py
+++ lecture_011/file.py
@@ -10,29 +10,55 @@ def allPermutation(str, vis, count, myAns, ans):
             allPermutation(str, vis, count + 1, myAns, ans + str[i])
             vis[i] = False
 
+
 def allPermutation():
     myAns = []
     vis = [False] * 3
-    allPermutation("abc",vis,0, myAns,"")
+    allPermutation("abc", vis, 0, myAns, "")
     print(myAns)
 
 
 # no extra space
-def allUniquePermutation(str, vis, count, myAns, ans):
-    return 1
+def allUniquePermutations(str, vis, count, myAns, ans):
+    if count == len(str):
+        myAns.append(ans)
+        return
+
+    prevChar = '$'
+    for i in range(len(str)):
+        if vis[i] == False and prevChar != str[i]:
+            vis[i] = True
+            allUniquePermutations(str, vis, count + 1, myAns, ans + str[i])
+            vis[i] = False
+            prevChar = str[i]
+
 
 def allUniquePermutation():
-    str = "aabaabb"
-    
-    str = ''.join(sorted(str))
+    str = "aaaaaaa"
+
+    str = ''.join(sorted(str))   # NLog(N)
     vis = [False] * len(str)
     myAns = []
 
-    allUniquePermutation(str,vis,0, myAns,"")
+    allUniquePermutations(str, vis, 0, myAns, "")
     print(myAns)
 
-# T: O(N), S: O(N)
+
+allUniquePermutation()
+
+# Leetcode 46 and 47 for you.
+
+
+# T: O(N), S: O(N), Constraints: str contains only alphabetical Characters
+def generateStringOnFreq(ch, freq):
+    return ""
+
+
 def sortString(str):
     ans = ""
-
     return ans
+
+# sr: starting Row, sc: starting Column, er: ending Row, ec: Ending Column,
+def mazePath(sr, sc, er, ec, ans, MyAns):
+
+    return -1
