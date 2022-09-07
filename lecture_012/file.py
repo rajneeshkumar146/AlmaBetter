98ce0b2e9987eb60020273450818ea1f2ed093a5
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-07 21:31:35 +0530

    update

--- lecture_012/file.py
+++ lecture_012/file.py
@@ -20,9 +20,43 @@ def floodFill_01(sr, sc, er, ec, vis, ans):
     # for up movement
     if sr - 1 >= 0 and sc + 0 >= 0 and vis[sr - 1][sc + 0] == False:
         count += floodFill_01(sr - 1, sc + 0, er, ec, vis, ans + 'u')
-    
+
     vis[sr][sc] = False
     return count
+
+
+def maximumMoves(sr, sc, er, ec, vis):
+    if sr == er and sc == ec:
+        return 0
+
+    vis[sr][sc] = True
+    moves = -1
+    # for right movement
+    if sr + 0 <= er and sc + 1 <= ec and vis[sr + 0][sc + 1] == False:
+        myMove = maximumMoves(sr + 0, sc + 1, er, ec, vis)
+        if myMove != -1:
+            moves = max(moves, myMove + 1)
+
+    # for left movement
+    if sr + 0 <= er and sc - 1 >= 0 and vis[sr + 0][sc - 1] == False:
+        myMove = maximumMoves(sr + 0, sc - 1, er, ec, vis)
+        if myMove != -1:
+            moves = max(moves, myMove + 1)
+
+    # for down movement
+    if sr + 1 <= er and sc + 0 <= ec and vis[sr + 1][sc + 0] == False:
+        myMove = maximumMoves(sr + 1, sc + 0, er, ec, vis)
+        if myMove != -1:
+            moves = max(moves, myMove + 1)
+
+    # for up movement
+    if sr - 1 >= 0 and sc + 0 >= 0 and vis[sr - 1][sc + 0] == False:
+        myMove = maximumMoves(sr - 1, sc + 0, er, ec, vis)
+        if myMove != -1:
+            moves = max(moves, myMove + 1)
+            
+    vis[sr][sc] = False
+    return moves
 
 
 def floodFill():
@@ -34,6 +68,8 @@ def floodFill():
             v.append(False)
         vis.append(v)
 
-    print(floodFill_01(sr,sc,er,ec,vis,""))
+    # print(floodFill_01(sr, sc, er, ec, vis, ""))
+    print(maximumMoves(sr,sc,er,ec,vis))
+
 
 floodFill()
