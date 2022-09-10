b40f637c3cf65ecfaccc42dd75612b51ffeffd01
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-10 12:22:57 +0530

    update

--- lecture_013/sorting.py
+++ lecture_013/sorting.py
@@ -20,6 +20,8 @@ def bubbleSort_opti(arr):
             break
 
 # TC = O(N2), SC = O(1)
+
+
 def selectionSort(arr):
     l = len(arr)
     for i in range(l):
@@ -27,4 +29,14 @@ def selectionSort(arr):
             if arr[i] < arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
 
+# TC in Avg case: O(K.N) where k is no of element which is not at there correct position, TC in Worst case: O(N^2)
+
+
 def insertionSort(arr):
+    l = len(arr)
+
+    for i in range(1, l):
+        j = i - 1
+        while j >= 0 and arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            j -= 1
