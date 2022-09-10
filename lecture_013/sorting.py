092c85ef27e91ec328bf85e7b48604c85522a261
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-10 11:21:37 +0530

    update

--- lecture_013/sorting.py
+++ lecture_013/sorting.py
@@ -1,4 +1,4 @@
-# TC?, SC?
+# TC = O(N2), SC = O(1)
 def bubbleSort(arr):
     l = len(arr)
     for i in range(l):
@@ -14,6 +14,11 @@ def bubbleSort_opti(arr):
         for j in range(1, l - i):
             if arr[j - 1] > arr[j]:
                 arr[j - 1], arr[j] = arr[j], arr[j - 1]
+                isSorted = False
 
         if isSorted:
             break
+
+# TC = O(N2), SC = O(1)
+def selectionSort(arr):
+    l = len(arr)
