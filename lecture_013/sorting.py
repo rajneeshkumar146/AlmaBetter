5d913c13455bbd542acb22e58ff2814350fb4ab4
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-10 11:43:55 +0530

    update

--- lecture_013/sorting.py
+++ lecture_013/sorting.py
@@ -22,3 +22,9 @@ def bubbleSort_opti(arr):
 # TC = O(N2), SC = O(1)
 def selectionSort(arr):
     l = len(arr)
+    for i in range(l):
+        for j in range(i + 1, l):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+def insertionSort(arr):
