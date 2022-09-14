5861c095b86198295b1ea1963fee5c656b80eb35
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-14 22:23:19 +0530

    update

--- lecture_014/001_file.py
+++ lecture_014/001_file.py
@@ -13,6 +13,7 @@ def sort01(arr):
 
         itr += 1
 
+
 def segregateElements(arr, index):
     data = arr[index]
     n, pt, itr = len(arr), -1, 0
@@ -22,14 +23,43 @@ def segregateElements(arr, index):
             swap(arr, pt, itr)
 
         itr += 1
-    
+
 # segregate your entire array in such a way that your index data will at there correct sorted place.
-def segregateElementsWithSortedPlace(arr, index):
-    data = arr[index]
+
+
+def segregateElements_02(arr, index):
     n, pt, itr = len(arr), -1, 0
+
+    data = arr[index]
+    swap(arr, index, n - 1)
+
     while itr < n:
         if arr[itr] <= data:
             pt += 1
             swap(arr, pt, itr)
 
         itr += 1
+
+    return pt
+
+# T = O(NLog(N)), Worst Case is: T(N2)
+# S =  O(Log(N)), Worrst Case is: T(N)
+def quickSort(arr, si, ei):
+    if si >= ei:
+        return
+
+    mid = (si + ei) // 2
+    pivotIdx = segregateElements_02(arr, mid)   # O(N)
+
+    quickSort(arr, si, pivotIdx - 1)
+    quickSort(arr, pivotIdx + 1, ei)
+
+
+def quickSort(arr):
+    n = len(arr)
+    quickSort(arr, 0, n - 1)
+
+
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+# T: O(NLogN)
+# S: O(LogN)
