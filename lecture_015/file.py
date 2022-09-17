401f9f2f802e0c9719fecd2e6ff4f2bacaf4689b
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-17 10:39:18 +0530

    update

---
+++ lecture_015/file.py
@@ -0,0 +1,57 @@
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
+
+def segregateElements_02(arr, index):
+    n, pt, itr = len(arr), -1, 0
+
+    data = arr[index]
+    swap(arr, index, n - 1)
+
+    while itr < n:
+        if arr[itr] <= data:
+            pt += 1
+            swap(arr, pt, itr)
+
+        itr += 1
+
+    return pt
+
+
+def isSubArraySorted(arr, si, ei):
+    return False
+
+
+# T = O(NLog(N)), Worst Case is: T(N2)
+# S =  O(Log(N)), Worrst Case is: T(N)
+def quickSort(arr, si, ei):
+    if si >= ei:
+        return
+
+    if isSubArraySorted(arr,si,ei):
+        return
+
+    mid = (si + ei) // 2
+    pivotIdx = segregateElements_02(arr, mid)   # O(N)
+
+    quickSort(arr, si, pivotIdx - 1)
+    quickSort(arr, pivotIdx + 1, ei)
+
+
+def quickSort(arr):
+    n = len(arr)
+    quickSort(arr, 0, n - 1)
+
+
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+# T: O(NLogN)
+# S: O(LogN)
+
+def kthLargest_01():
+
+
+def kthLargest():
+    
+    
