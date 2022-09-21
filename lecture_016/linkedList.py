8b00b9d930a12700114faf3760c725fa344f5dd0
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-09-21 20:54:09 +0530

    update

--- lecture_016/linkedList.py
+++ lecture_016/linkedList.py
@@ -2,24 +2,37 @@ class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
-        
+
+
 class MyLinkedList:
 
     def __init__(self):
         self.head = None
         self.tail = None
         self.size = 0
-        
-        
+
     def getFirst(self):
         return self.head.val
-        
+
     def getLast(self):
         return self.tail.val
 
+    def getNodeAt(self, index):
+        curr = self.head
+        while index > 0:
+            curr = curr.next
+            index -= 1
+
+        return curr
+
     def get(self, index: int) -> int:
-    
-    #addFirst
+        if index < 0 or index >= self.size:
+            return -1
+
+        node = self.getNodeAt(index)
+        return node.val
+
+    # addFirst
     def addAtHead(self, val: int) -> None:
         newNode = ListNode(val)
         if self.head is None:
@@ -28,11 +41,10 @@ class MyLinkedList:
         else:
             newNode.next = self.head
             self.head = newNode
-        
+
         self.size += 1
 
-
-    #addTail
+    # addTail
     def addAtTail(self, val: int) -> None:
         newNode = ListNode(val)
         if self.head is None:
@@ -44,24 +56,12 @@ class MyLinkedList:
 
         self.size += 1
 
-        
-
-    #addAt(index)
+    # addAt(index)
     def addAtIndex(self, index: int, val: int) -> None:
-        
+
     def deleteFirst(self):
-        
+
     def deleteLast(self):
-        
-    #DeleteAt(index)
+
+    # DeleteAt(index)
     def deleteAtIndex(self, index: int) -> None:
-        
-
-
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
